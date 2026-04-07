3c5e0378afb7de9ad7f2ca52202acbd7950a6a89
DanEra1998 <danellegendary1998@gmail.com>
2026-04-07 16:24:53 -0700

    Addition of NotificationEventArgs model

---
+++ NotificationSystem/Models/NotificationEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace NotificationSystem.Models
+{
+    public class NotificationEventArgs : EventArgs
+    {
+        public string RecipientName {  get; set; }
+        public string Message { get; set; } 
+        public DateTime Timestamp { get; set; }
+
+        public NotificationEventArgs(string recipientName, string message)
+        {
+            RecipientName = recipientName;
+            Message = message;  
+            Timestamp = DateTime.Now;
+
+        }
+    }
+}
